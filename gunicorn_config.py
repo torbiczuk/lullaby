bbe21367d67a460693f7c7b0c6bb03f5077dcc85
Robert Torbiczuk <r.torbiczuk@edrone.me>
2025-09-09 20:05:25 +0200

    Gunicorn 1 worker

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,7 +6,7 @@ bind = f"0.0.0.0:{os.environ.get('PORT', 5000)}"
 backlog = 2048
 
 # Worker processes
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 worker_class = "sync"
 worker_connections = 1000
 timeout = 30
